c2c465be9b3f39c8e1cb25bcb373412681fb61ce
dargonesti <migneault62@hotmail.com>
2020-08-11 22:23:57 -0400

    Triangle animé header v1

--- src/views/LandingPage/LandingPageNoAnim.jsx
+++ src/views/LandingPage/LandingPageNoAnim.jsx
@@ -42,16 +42,16 @@ const LandingPage = ({ loaded, showTests, ...props }) => {
   let titre = translatedTxt.landingTitre;
 
   let imgWedding = ["3_XT034714.jpg", "2_XT034657.jpg", "3_XT034770 (1).jpg", "3_XT034778.jpg", "4_XT033079 (2).jpg", "5_XT033095 (2).jpg"]
-  let getWD = (i) => ("url(img/Wedding/" + imgWedding[i % imgWedding.length] + ")")
+  let getWD = (i) => ("url(img/Wedding/" + imgWedding[i % imgWedding.length] + ")")//linear-gradient(to right bottom, rgba(0,200,123, 0.3), rgba(223,234,94,0.2)),
 
   let imgPortrait = ["4_XT201199 (1).jpg", "5_DSCF5764 (1).jpg", "5_DSCF5807.jpg", "3_XT032214.webp"]
-  let getP = (i) => ("url(img/Portraits/" + imgPortrait[i % imgPortrait.length] + ")")
+  let getP = (i) => (" url(img/Portraits/" + imgPortrait[i % imgPortrait.length] + ")")//linear-gradient(to right bottom, rgba(200,0,1, 0.2), rgba(123,0,234,0.2)),
 
   let imgNature = ["0_XT035256.jpg", "4_XT032371 (1).webp", "4_XT032376.webp", "5_XT032153.jpg", "DSCF9114_4.jpg"]
-  let getN = (i) => ("url(img/Nature/" + imgNature[i % imgNature.length] + ")")
+  let getN = (i) => ("url(img/Nature/" + imgNature[i % imgNature.length] + ")")//linear-gradient(to right bottom, rgba(0,200,123, 0.3), rgba(223,234,94,0.2)),
 
   let selfPortrait = ["4_XT031878.jpg", "3_XT031397.webp", "3_XT031376.jpg", "redBlueBall.jpg"]
-  let getSP = (i) => ("url(img/PortraitsMoi/" + selfPortrait[i % selfPortrait.length] + ")")
+  let getSP = (i) => ("url(img/PortraitsMoi/" + selfPortrait[i % selfPortrait.length] + ")")//linear-gradient(to right bottom, rgba(234,234,234, 0.3), rgba(234,234,99,0.3)),
 
 
   useEffect(() => {
